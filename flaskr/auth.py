470a28746ef753237b734eb9acbf18b9e12a903f
maximesunnen <maximesunnen@gmail.com>
2023-10-18 17:10:29 +0200

    Added a login decorator

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -95,3 +95,14 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+# Define login decorator
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
