d49315958f4f168c0356c8b9b64be4a4cf7bb4aa
maximesunnen <maximesunnen@gmail.com>
2023-11-14 09:54:38 +0100

    Commented or removed code for old login and register views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,4 @@
-import functools
+""" import functools
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -143,3 +143,4 @@ def login_required(view):
 def account():
     return render_template("auth/account.html")
 
+ """
--- flaskr/index.py
+++ flaskr/index.py
@@ -4,19 +4,12 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from flaskr.db import get_db
-
-from flaskr.auth import login_required
-
+#from flaskr.auth import login_required
 from .helpers.form_helpers import form_filled_in
-
 from flaskr.forms import userForm, tutorForm, urgentForm, questionsForm
-
 from .mail import mail
-
 from flask_mail import Message
-
 from collections import OrderedDict
 
 bp = Blueprint("index", __name__)
