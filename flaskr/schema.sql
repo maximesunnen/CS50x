b95116c8b9604ffe983f03ed5b35e7b5dd3209db
maximesunnen <maximesunnen@gmail.com>
2023-11-14 09:29:35 +0100

    Restructured the database, combined two tables into one; reshaped backend code accordingly

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS parent;
-DROP TABLE IF EXISTS parent_child;
+DROP TABLE IF EXISTS emergency;
+DROP TABLE IF EXISTS parent_child_emergency;
 
 -- Table to store user (child) information
 CREATE TABLE user (
@@ -53,18 +54,12 @@ composite key: the combination of (parent_id, child_id) must be unique. For exam
 junction table: create a many-to-many relationship between entitities (one parent -> many users, one user -> many (>1, usually) parents)
 */
 
-CREATE TABLE parent_child (
+CREATE TABLE parent_child_emergency (
   parent_id INTEGER NOT NULL,
   child_id INTEGER NOT NULL,
-  PRIMARY KEY (parent_id, child_id),
-  FOREIGN KEY (parent_id) REFERENCES parent (id),
-  FOREIGN KEY (child_id) REFERENCES user (id)
-);
-
-CREATE TABLE emergency_child (
   emergency_id INTEGER NOT NULL,
-  child_id INTEGER NOT NULL,
-  PRIMARY KEY (emergency_id, child_id),
-  FOREIGN KEY (emergency_id) REFERENCES emergency (id),
-  FOREIGN KEY (child_id) REFERENCES user (id)
+  PRIMARY KEY (parent_id, child_id, emergency_id),
+  FOREIGN KEY (parent_id) REFERENCES parent (id),
+  FOREIGN KEY (child_id) REFERENCES user (id),
+  FOREIGN KEY (emergency_id) REFERENCES emergency (id)
 );
