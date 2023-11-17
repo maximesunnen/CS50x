b0d6bbdbf40e4451cbd4c2ab717c3b7b64a8b6de
maximesunnen <maximesunnen@gmail.com>
2023-11-17 11:20:02 +0100

    Added classmethod from_dict; added curation logic for birthday attribute

--- flaskr/classes.py
+++ flaskr/classes.py
@@ -4,7 +4,12 @@ class Scout:
     def __init__(self, first_name, last_name, birthday, gender, number, branch, email, house_number, street, town, zip, country, allergies, diet, other):
         self.first_name = first_name.upper()
         self.last_name = last_name.upper()
-        self.birthday = birthday.strftime("%Y-%m-%d")
+        
+        if isinstance(birthday, str):
+            self.birthday = birthday
+        else:
+            self.birthday = birthday.strftime("%Y-%m-%d")
+            
         self.gender = gender.upper()
         self.number = number
         self.branch = branch
@@ -18,16 +23,24 @@ class Scout:
         self.diet = diet
         self.other = other
         
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+        
 class Tutor:
     def __init__(self, first_name, last_name, number, email):
         self.first_name = first_name.upper()
         self.last_name = last_name.upper()
         self.number = number
         self.email = email
-
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+        
+from collections import OrderedDict
 class CustomEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, (Scout, Tutor)):
-            return obj.__dict__
+            return OrderedDict(obj.__dict__)
         return super().default(obj)
-        
